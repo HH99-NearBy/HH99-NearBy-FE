dd1e8be64c07359f7280b14f7e1b93844d5e4ccd
TaeTaehoon <rad572985@gmail.com>
2022-08-31 23:45:11 +0900

    add: project initial directory setting

---
+++ src/pages/ChallengingPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function ChallengingPage() {}
+
+export default ChallengingPage;
---
+++ src/routers/Router.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+function Router() {
+  return <Routes></Routes>;
+}
+
+export default Router;
